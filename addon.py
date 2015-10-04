6f1cc6d1defeb4b2ea6178f01ce57bea07525e83
zchira <zchira@gmail.com>
2015-10-04 19:12:24 +0200

    added buildzip.sh and modified adon.py'

--- addon.py
+++ addon.py
@@ -1,13 +1,39 @@
 import sys
+import urllib
+import urlparse
 import xbmcgui
 import xbmcplugin
 
+base_url = sys.argv[0]
 addon_handle = int(sys.argv[1])
+args = urlparse.parse_qs(sys.argv[2][1:])
+
 
 xbmcplugin.setContent(addon_handle, 'movies')
 
-url = 'http://best.str.n1info.com:8080/stream?sp=n1info&channel=n1srb&stream=1mb&b=6&player=m3u8&u=n1info&p=n1Sh4redSecre7iNf0'
-li = xbmcgui.ListItem('N1 RS!', iconImage='icon.png')
-xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+def build_url(query):
+    return base_url + '?' + urllib.urlencode(query)
 
-xbmcplugin.endOfDirectory(addon_handle)
+mode = args.get('mode', None)
+
+if (mode) is None:
+    url = build_url({'mode': 'folder', 'foldername':'Folder One'})
+    li = xbmcgui.ListItem('Folder One', iconImage='DefaultFolder.png')
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url,
+                                listitem=li, isFolder=True)
+
+    url = build_url({'mode': 'folder', 'foldername':'Folder Two'})
+    li = xbmcgui.ListItem('Folder Two', iconImage='DefaultFolder.png')
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url,
+                                listitem=li, isFolder=True)
+
+
+    xbmcplugin.endOfDirectory(addon_handle)
+
+elif mode[0] == 'folder':
+    foldername = args['foldername'][0]
+    url = 'http://best.str.n1info.com:8080/stream?sp=n1info&channel=n1srb&stream=1mb&b=6&player=m3u8&u=n1info&p=n1Sh4redSecre7iNf0'
+    # li = xbmcgui.ListItem('N1 RS!', iconImage='icon.png')
+    li = xbmcgui.ListItem(foldername + ' Video', iconImage='DefaultVideo.png')
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+    xbmcplugin.endOfDirectory(addon_handle)
---
+++ buildzip.sh
@@ -0,0 +1,6 @@
+mv plugin.video.n1exyu.zip plugin.video.n1exyu.zip.old
+cd ..
+zip -r plugin.video.n1exyu.zip plugin.video.n1exyu/LICENSE plugin.video.n1exyu/README.md plugin.video.n1exyu/addon.py plugin.video.n1exyu/addon.xml plugin.video.n1exyu/changelog.txt plugin.video.n1exyu/icon.png plugin.video.n1exyu/resources/
+
+cp plugin.video.n1exyu.zip plugin.video.n1exyu/
+cd plugin.video.n1exyu/
