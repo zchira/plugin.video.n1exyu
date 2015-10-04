50c094d517e684864a0037d97b17538af35cf879
zchira <zchira@gmail.com>
2015-10-04 20:00:19 +0200

    files moved to folders

--- buildzip.sh
+++ buildzip.sh
@@ -1,6 +1,6 @@
-mv plugin.video.n1exyu.zip plugin.video.n1exyu.zip.old
-cd ..
-zip -r plugin.video.n1exyu.zip plugin.video.n1exyu/LICENSE plugin.video.n1exyu/README.md plugin.video.n1exyu/addon.py plugin.video.n1exyu/addon.xml plugin.video.n1exyu/changelog.txt plugin.video.n1exyu/icon.png plugin.video.n1exyu/resources/
+mv builds/plugin.video.n1exyu.zip builds/plugin.video.n1exyu.zip.old
 
-cp plugin.video.n1exyu.zip plugin.video.n1exyu/
-cd plugin.video.n1exyu/
+zip -r plugin.video.n1exyu.zip plugin.video.n1exyu/
+
+mv plugin.video.n1exyu.zip builds/
+
---
+++ deploy.sh
@@ -0,0 +1,1 @@
+scp builds/plugin.video.n1exyu.zip  'pi@192.168.1.200:/media/USBHDD1/shares/kodiPlugin'
