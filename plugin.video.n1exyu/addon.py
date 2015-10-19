3ad348db2ec4ff770b28af91b03be14d03b08b1a
zchira <zchira@gmail.com>
2015-10-20 01:23:03 +0200

    added streams for serbian, croatian, and bosnian regions

--- plugin.video.n1exyu/addon.py
+++ plugin.video.n1exyu/addon.py
@@ -3,6 +3,7 @@ import urllib
 import urlparse
 import xbmcgui
 import xbmcplugin
+import xbmcaddon
 
 base_url = sys.argv[0]
 addon_handle = int(sys.argv[1])
@@ -20,13 +21,31 @@ def youtube_url(videoid):
 
 mode = args.get('mode', None)
 
-print "mode"
+#print "mode"
+my_addon = xbmcaddon.Addon("plugin.video.n1exyu")
+iconImage = my_addon.getAddonInfo('icon')
+fanart = my_addon.getAddonInfo('fanart')
 
 if (mode) is None:
     url = 'http://best.str.n1info.com:8080/stream?sp=n1info&channel=n1srb&stream=1mb&b=6&player=m3u8&u=n1info&p=n1Sh4redSecre7iNf0'
-    li = xbmcgui.ListItem('N1 Live', iconImage='icon.png')
+    li = xbmcgui.ListItem('N1 Live [Serbia]', iconImage=iconImage)
+    li.setProperty('isplayable', 'true')
+    li.setProperty('fanart_image', fanart)
     xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
 
+    url = 'http://best.str.n1info.com:8080/stream?sp=n1info&channel=n1hr&stream=1mb&b=6&player=m3u8&u=n1info&p=n1Sh4redSecre7iNf0'
+    li = xbmcgui.ListItem('N1 Live [Croatia]', iconImage=iconImage)
+    li.setProperty('isplayable', 'true')
+    li.setProperty('fanart_image', fanart)
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+    url = 'http://best.str.n1info.com:8080/stream?sp=n1info&channel=n1bih&stream=1mb&b=6&player=m3u8&u=n1info&p=n1Sh4redSecre7iNf0'
+    li = xbmcgui.ListItem('N1 Live [BiH]', iconImage=iconImage)
+    li.setProperty('isplayable', 'true')
+    li.setProperty('fanart_image', fanart)
+    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url, listitem=li)
+
+    
 #    url = build_url({'mode': 'folder', 'foldername':'Pressing'})
 #    li = xbmcgui.ListItem('Pressing', iconImage='icon.png')
 #    xbmcplugin.addDirectoryItem(handle=addon_handle, url=url,
